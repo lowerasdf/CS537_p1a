d927cea186388e45977a8ac01564ceda71cd893c
lowerasdf <bjchandra@wisc.edu>
2021-09-13 20:54:39 -0500

    I/O done

--- kv.c
+++ kv.c
@@ -5,9 +5,9 @@
 
 const char *db_name = "database.txt";
 const int hash_capacity = 1000;
+const int buffer_size = 1000;
 
-struct Node
-{
+struct Node {
     int key;
     char *val;
     struct Node *next;
@@ -15,15 +15,13 @@ struct Node
 
 void printList(struct Node* root) {
     struct Node *curr = root;
-    while (curr != NULL)
-    {
+    while (curr != NULL) {
         printf("%d:%s\n", curr->key, curr->val);
         curr = curr->next;
     }
 }
 
-void appendOrSet(struct Node** root, int new_key, char *new_val)
-{
+void appendOrSet(struct Node** root, int new_key, char *new_val) {
     if (root == NULL) {
         printf("appendOrSet: pointer to root should not be null\n");
         return;
@@ -35,24 +33,20 @@ void appendOrSet(struct Node** root, int new_key, char *new_val)
     new_node->val = new_val;
     new_node->next = NULL;
 
-    if (*root == NULL)
-    {
+    if (*root == NULL) {
         *root = new_node;
         return;
     }
 
-    while (curr->next != NULL)
-    {
-        if (curr->key == new_key)
-        {
+    while (curr->next != NULL) {
+        if (curr->key == new_key) {
             free(new_node);
             curr->val = new_val;
             return;
         }
         curr = curr->next;
     }
-    if (curr->key == new_key)
-    {
+    if (curr->key == new_key) {
         free(new_node);
         curr->val = new_val;
         return;
@@ -62,13 +56,10 @@ void appendOrSet(struct Node** root, int new_key, char *new_val)
     return;
 }
 
-char *get(struct Node* root, int key)
-{
+char* get(struct Node* root, int key) {
     struct Node *curr = root;
-    while (curr != NULL)
-    {
-        if (curr->key == key)
-        {
+    while (curr != NULL) {
+        if (curr->key == key) {
             return curr->val;
         }
         curr = curr->next;
@@ -76,8 +67,7 @@ char *get(struct Node* root, int key)
     return NULL;
 }
 
-int deleteNode(struct Node** root, int key)
-{
+int deleteNode(struct Node** root, int key) {
     if (root == NULL) {
         printf("deleteNode: pointer to root should not be null\n");
         return 0;
@@ -106,62 +96,91 @@ int deleteNode(struct Node** root, int key)
     return 1;
 }
 
-void freeAll(struct Node* node)
-{
-    if (node == NULL)
-    {
+void freeAll(struct Node* node) {
+    if (node == NULL) {
         return;
     }
     freeAll(node->next);
     free(node);
 }
 
-int hash(int key)
-{
+int hash(int key) {
     return key % hash_capacity;
 }
 
-int main(int argc, char *argv[])
-{
-    // // TODO hash table
+int main(int argc, char *argv[]) {
+    // hash table
+    struct Node *hash_table[hash_capacity];
+    for(int i = 0; i < hash_capacity; i++) {
+        hash_table[i] = NULL;
+    }
 
-    // // read db
-    // FILE *file = fopen(db_name, "r");
-    // if (file != NULL) {
-    //     char *line = NULL;
-    //     size_t linecap = 0;
-    //     ssize_t linelen;
-    //     while((linelen = getline(&line, &linecap, file)) > 0) {
-    //         printf("input: %s", line);
-    //     }
+    // read db
+    FILE *file = fopen(db_name, "r");
+    if (file != NULL) {
+        char *line = NULL;
+        size_t linecap = 0;
+        ssize_t linelen;
+        while((linelen = getline(&line, &linecap, file)) > 1) {
+            char *token;
+            int temp = 0;
+            int key;
+            char *val;
+            while((token = strsep(&line, ",")) != NULL) {
+                if (temp == 0) {
+                    key = atoi(token);
+                } else if (temp == 1) {
+                    token[strcspn(token, "\n")] = 0;
+                    val = token;
+                }
+                temp += 1;
+            }
 
-    //     // TODO add to hash table
+            int hash_code = hash(key);
+            appendOrSet(&hash_table[hash_code], key, val);
+        }
 
-    //     fclose(file);
-    // }
+        fclose(file);
+    }
 
-    // for(int i = 1; i < argc; i++) {
-    //     printf("%s\n", argv[i]);
+    for(int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
 
-    //     char *token, *string, *tofree;
+        char *token, *string, *tofree;
+        tofree = string = strdup(argv[i]);
+        assert(string != NULL);
 
-    //     tofree = string = strdup(argv[i]);
-    //     assert(string != NULL);
+        while((token = strsep(&string, ",")) != NULL) {
+            printf("  token:%s\n", token);
 
-    //     while((token = strsep(&string, ",")) != NULL) {
-    //         printf("  token:%s\n", token);
+            // TODO check validity
+        }
 
-    //         // TODO check validity
-    //     }
+        free(tofree);
 
-    //     free(tofree);
+        // TODO if valid do something
+    }
 
-    //     // TODO if valid do something
-    // }
+    // store db
+    FILE *output_file = fopen(db_name, "w");
+    assert(output_file != NULL);
 
-    // // TODO store db
-    // FILE *output_file = fopen(db_name, "w");
-    // assert(output_file != NULL);
+    for(int i = 0; i < hash_capacity; i++) {
+        if (hash_table[i] != NULL) {
+            struct Node *curr = hash_table[i];
+            while(curr != NULL) {
+                fprintf(output_file,"%d,%s\n", curr->key, curr->val);                
+                curr = curr->next;
+            }
+        }
+    }
+
+    fclose(output_file);
+
+    // free hash table
+    for(int i = 0; i < hash_capacity; i++) {
+        freeAll(hash_table[i]);
+    }
 
     // struct Node *root = NULL;
     // appendOrSet(&root, 1, "hello");
@@ -176,20 +195,18 @@ int main(int argc, char *argv[])
 
     // int keys[] = {1, 3, 2, 4, 1004, 1002};
     // char *vals[] = {"hello", "none", "python", "go", "java", "c"};
-    int keys[] = {4, 1004};
-    char *vals[] = {"hello", "none"};
-    struct Node *hash_table[hash_capacity];
-    for(int i = 0; i < hash_capacity; i++) {
-        hash_table[i] = NULL;
-    }
-    for(int i = 0; i < (sizeof keys / sizeof *keys); i++) {
-        int hash_code = hash(keys[i]);
-        appendOrSet(&hash_table[hash_code], keys[i], vals[i]);
-    }
+    // struct Node *hash_table[hash_capacity];
+    // for(int i = 0; i < hash_capacity; i++) {
+    //     hash_table[i] = NULL;
+    // }
+    // for(int i = 0; i < (sizeof keys / sizeof *keys); i++) {
+    //     int hash_code = hash(keys[i]);
+    //     appendOrSet(&hash_table[hash_code], keys[i], vals[i]);
+    // }
 
-    printList(hash_table[4]);
+    // printList(hash_table[4]);
     
-    for(int i = 0; i < hash_capacity; i++) {
-        freeAll(hash_table[i]);
-    }
+    // for(int i = 0; i < hash_capacity; i++) {
+    //     freeAll(hash_table[i]);
+    // }
 }
